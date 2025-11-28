cbb7204ceffbec9c0620b6640e16ef33d14e1061
sammer2605 <authorsamwhitaker@gmail.com>
2025-11-28 16:34:24 -0700

    Add Arduino Motor Class, MotionTimelapse Class

--- devices.py
+++ devices.py
@@ -1,6 +1,7 @@
 import subprocess
 import fractions
 import time
+import serial
 
 class Intervalometer:
     def __init__(self, numShots, interval, default_exposure=1.0):
@@ -10,7 +11,10 @@ class Intervalometer:
         self.num_shots = numShots
         self.interval = interval
         self.default_exposure = default_exposure
+        # self.steps_per_shot = steps_per_shot
         self.set_capture_target()
+
+        # self.slider = StepperSlider(port=slider_port)
     
     # Camera Control Methods
     def set_capture_target(self):
@@ -67,3 +71,43 @@ class Intervalometer:
             time.sleep(exposure) # Wait for exposure to finish
             if i != self.num_shots - 1:
                 time.sleep(self.interval)
+
+class StepperSlider:
+    def __init__(self, port="/dev/ttyACM0", baudrate=115200, timeout=2):
+        self.ser = serial.Serial(port, baudrate = baudrate, timeout = timeout)
+        time.sleep(2) # Wait for Arduino to reset
+
+    def move_steps(self, steps):
+        """Move the motor a specific number of steps. Positive = forward."""
+        command = f"MOVE {steps}\n"
+        self.ser.write(command.encode())
+
+        # Wait for Arduino to confirm move is done
+        while True:
+            line = self.ser.readline().decode().strip()
+            if line == "DONE":
+                break
+    
+    def close(self):
+        self.ser.close()
+
+class MotionTimelapse:
+    def __init__(self, num_shots, interval, default_exposure=1.0, steps_per_shot=100, slider_port="/dev/ttyACM0"):
+        self.intervalometer = Intervalometer(num_shots, interval, default_exposure)
+        self.slider = StepperSlider(port=slider_port)
+        self.steps_per_shot = steps_per_shot
+        self.num_shots = num_shots
+
+    def run(self):
+        for i in range(self.num_shots):
+            exposure = self.intervalometer.get_shutter_speed()
+            print(f"Shot {i+1}/{self.num_shots} | Exposure: {exposure:.3f}s")
+            self.intervalometer.trigger_shutter()
+            time.sleep(exposure)
+            if i != self.num_shots - 1:
+                print(f"Moving slider {self.steps_per_shot} steps")
+                self.slider.move_steps(self.steps_per_shot)
+                time.sleep(max(0, self.intervalometer.interval - exposure))
+
+    def close(self):
+        self.slider.close()
